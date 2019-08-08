8cc68d623e9c91c0837850b4bc82ce7345300fb8
Mark Randall <mrandall@nerdery.com>
2019-08-08 06:52:12 -0500

    Initial Commit

---
+++ MessagingPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessagingPOC
+//
+//  Created by Mark Randall on 8/8/19.
+//  Copyright © 2019 Mark Randall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
